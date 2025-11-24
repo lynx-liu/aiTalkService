904569b508150abed8305022764373b98522985c
lynx <13651417694@126.com>
2025-11-25 00:09:28 +0800

    设置日志实时输出不缓存

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,9 @@
 CONFIG ServerConfig;
 int main(int argc, char* argv[])
 {
+    setvbuf(stdout, NULL, _IONBF, 0);//设置printf无缓冲,否则默认要等到换行或缓冲区满才输出
+    setvbuf(stderr, NULL, _IONBF, 0);
+
     get_config(&ServerConfig);
 
     signal(SIGPIPE, SIG_IGN);
